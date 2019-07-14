80b7f41c04cf1a9e4f10dc790688418a828f7ab2
Tiago Alves <=>
2019-07-14 00:24:07 -0300

    - Add dockerfile - Init INPUT.json folders - Add initial implementation in crawler

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+# Dockerfile contains instructions how to build a Docker image that will contain
+# all the code and configuration needed to run your actor. For a full
+# Dockerfile reference, see https://docs.docker.com/engine/reference/builder/
+
+# First, specify the base Docker image. Apify provides the following base images
+# for your convenience:
+#  apify/actor-node-basic (Node.js 10 on Alpine Linux, small and fast image)
+#  apify/actor-node-chrome (Node.js 10 + Chrome on Debian)
+#  apify/actor-node-chrome-xvfb (Node.js 10 + Chrome + Xvfb on Debian)
+# For more information, see https://apify.com/docs/actor#base-images
+# Note that you can use any other image from Docker Hub.
+FROM apify/actor-node-chrome
+
+# Copy all files and directories with the source code
+COPY . ./
+
+# Install NPM packages, skip optional and development dependencies to keep the
+# image small. Avoid logging to much and print the dependency tree for debugging
+RUN npm --quiet set progress=false \
+ && npm install --only=prod --no-optional \
+ && echo "Installed NPM packages:" \
+ && npm list \
+ && echo "Node.js version:" \
+ && node --version \
+ && echo "NPM version:" \
+ && npm --version
+
+# Specify how to run the source code
+CMD npm start
--- src/crawler.js
+++ src/crawler.js
@@ -1,15 +1,68 @@
-import Apify from 'apify';
+
+// This is the main Node.js source code file of your actor.
+// It is referenced from the "scripts" section of the package.json file,
+// so that it can be started by running "npm start".
+
+const Apify = require('apify');
 
 Apify.main(async () => {
+    // Get input of the actor.
+    // If you'd like to have your input checked and generate a user interface
+    // for it, add INPUT_SCHEMA.json file to your actor.
+    // For more information, see https://apify.com/docs/actor/input-schema
+    const input = await Apify.getInput();
+    console.log('Input:');
+    console.dir(input);
 
-	console.log('message => HELLO APIFY!');
+	const requestQueue = await Apify.openRequestQueue();
+    const pseudoUrls = [new Apify.PseudoUrl('https://www.iana.org/[.*]')];
 
-	function pageFunction(context) {
-		let JQuery = context.JQuery;
+    await requestQueue.addRequest(new Apify.Request({
+        url: 'https://eadgraduacao.ftec.com.br/login/index.php'
+    }));
 
-		JQuery.ajax({
-			url: 'https://eadgraduacao.ftec.com.br/login/index.php'
-		});
+    const crawler = new Apify.PuppeteerCrawler({
+        requestQueue,
 
-	}
+        handlePageFunction: async ({
+            request,
+            page
+        }) => {
+            const title = await page.title();
+            let selector = 'a';
+            console.log(`Title of ${request.url}: ${title}`);çç
+
+            await Apify.utils.enqueueLinks({
+                page,
+                selector,
+                pseudoUrls,
+                requestQueue
+            });
+        },
+
+        handleFailedRequestFunction: async ({
+            request
+        }) => {
+            console.log(`Request ${request.url} failed too many times`);
+
+            await Apify.pushData({
+                '#debug': Apify.utils.createRequestDebugInfo(request),
+            })
+
+        },
+        maxRequestsPerCrawl: 50,
+        maxConcurrency: 10,
+    });
+
+
+    // Save output
+    const output = {
+        receivedInput: input,
+        message: 'Hello sir!',
+    };
+    console.log('Output:');
+    console.dir(output);
+	await Apify.setValue('OUTPUT', output);
+
+    await crawler.run();
 });
